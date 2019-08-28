1dbff2bd17d34f53cf89cf0ef997e04003020ea4
Wilco Tubog <user@mein-arbeitsplatz.mshome.net>
2019-08-28 12:04:12 +0800

    chore: more cleanups

--- src/ngx-ez-maps/app-init.service.ts
+++ src/ngx-ez-maps/app-init.service.ts
@@ -1,30 +1,30 @@
-import { Observable } from 'rxjs/Observable';
-import { AppConfig, EZ_MAP_CONFIG } from './models/app-config.model';
-import { Injectable, Inject } from "@angular/core";
-
+import { Inject, Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { filter } from 'rxjs/operators';
+import { AppConfig, EZ_MAP_CONFIG } from './models/app-config.model';
+
 
 @Injectable()
 export class AppInitService {
-    private _hasInitialized = new BehaviorSubject<boolean>(false);
-    isGooglemapsReady = this._hasInitialized.asObservable().pipe(filter((hasInit) => hasInit));
+  private _hasInitialized = new BehaviorSubject<boolean>(false);
+  isGooglemapsReady = this._hasInitialized
+    .asObservable()
+    .pipe(filter(hasInit => hasInit));
 
-    constructor(@Inject(EZ_MAP_CONFIG) private conf: AppConfig) {}
+  constructor(@Inject(EZ_MAP_CONFIG) private conf: AppConfig) {}
 
-    initApp(){
-        if(!this._hasInitialized.getValue()){
-            const script = document.createElement('script');
-            script.src = `https://maps.googleapis.com/maps/api/js?key=${this.conf.apiKey}&libraries=${this.conf.libraries}&callback=ezMapInit`;
+  initApp() {
+    if (!this._hasInitialized.getValue()) {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${this.conf.apiKey}&libraries=${this.conf.libraries}&callback=ezMapInit`;
 
+      window['ezMapInit'] = () => {
+        this._hasInitialized.next(true);
+      };
 
-            window['ezMapInit'] = () => {
-                this._hasInitialized.next(true);
-            }
-
-            document.getElementsByTagName("head")[0].appendChild(script);
-        } else {
-          console.log('Maps Library has already initialized');
-        }
+      document.getElementsByTagName('head')[0].appendChild(script);
+    } else {
+      console.log('Maps Library has already initialized');
     }
+  }
 }
